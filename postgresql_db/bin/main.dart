86eea577d286cfc253788a36b6d930732e6e38d8
alireza easazade <easazade@gmail.com>
2022-01-23 23:10:49 +0330

    feat(postgresql_db): Add more examples on postgres library

--- postgresql_db/bin/main.dart
+++ postgresql_db/bin/main.dart
@@ -9,7 +9,17 @@ void main(List<String> arguments) async {
   await connection.open();
   print('connected to postgres database');
 
-  // create a table
+  // create a table orders
+  // await connection.query('''
+  // CREATE TABLE orders(
+  //   id serial primary key not null,
+  //   order_id int not null,
+  //   customer_id int not null,
+  //   order_date date
+  // )
+  // ''');
+
+  // create a table customers
   // await connection.query('''
   // CREATE TABLE customers(
   //   id serial primary key not null,
@@ -42,11 +52,33 @@ void main(List<String> arguments) async {
   // delete data
   await connection.query('DELETE FROM customers WHERE id > 8');
 
+  // insert our fake data into database
   await _insertFakeCustomersInDatabase(connection, doAllInsertionsInOneTransaction: true);
+  await _insertFakeOrdersInDatabase(connection, doAllInsertionsInOneTransaction: true);
+
+  // do a join query
+  var joinResult = await connection.mappedResultsQuery('''
+  SELECT customers.name, orders.order_id
+  from customers
+  LEFT JOIN orders on customers.id = orders.customer_id
+  ORDER BY customers.name
+  ''');
+  print('############ join result ##############');
+  print(joinResult);
 
   // if we don't close connection process won't stop
   await connection.close();
 }
+
+/// ################################# functions ######################################
+/// ################################# functions ######################################
+/// ################################# functions ######################################
+/// ################################# functions ######################################
+/// ################################# functions ######################################
+/// ################################# functions ######################################
+/// ################################# functions ######################################
+/// ################################# functions ######################################
+/// ################################# functions ######################################
 
 Future _insertFakeCustomersInDatabase(
   PostgreSQLConnection connection, {
@@ -68,6 +100,38 @@ Future _insertFakeCustomersInDatabase(
         'email': json['email'],
         'address': json['address'],
         'country': json['country'],
+      });
+    }
+  }
+
+  if (doAllInsertionsInOneTransaction) {
+    await connection.transaction((ctx) async {
+      await doInsertions(ctx);
+    });
+  } else {
+    await doInsertions(connection);
+  }
+}
+
+Future _insertFakeOrdersInDatabase(
+  PostgreSQLConnection connection, {
+  bool doAllInsertionsInOneTransaction = true,
+}) async {
+  final jsonStr = await File('mock_orders.json').readAsString();
+  final jsonArray = jsonDecode(jsonStr);
+
+  final mockDataStream = Stream.fromIterable(jsonArray);
+
+  Future doInsertions(PostgreSQLExecutionContext connectionContext) async {
+    await for (var json in mockDataStream) {
+      // we can use string substitution instead string interpolation
+      await connectionContext.query('''
+    INSERT INTO orders (order_id, customer_id, order_date)
+    VALUES (@order_id, @customer_id, @order_date)
+    ''', substitutionValues: {
+        'order_id': json['order_id'],
+        'customer_id': json['customer_id'],
+        'order_date': json['order_date'],
       });
     }
   }

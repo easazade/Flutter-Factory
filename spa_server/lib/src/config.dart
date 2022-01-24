2a6ad3ac022d6ca065d5292e2bd323ac5a6eb224
alireza easazade <easazade@gmail.com>
2022-01-24 22:30:01 +0330

    feat(spa_server): Add example env variables configs

--- spa_server/lib/src/config.dart
+++ spa_server/lib/src/config.dart
@@ -2,8 +2,16 @@ import 'package:envify/envify.dart';
 
 part 'config.g.dart';
 
-@Envify()
+@Envify(path: '.env')
 abstract class Env {
   static const secretKey = _Env.secretKey;
   static const mongoUrl = _Env.mongoUrl;
 }
+
+
+// we can define prod and test and staging if we need different env classes like below 
+// @Envify(path: '.env.development')
+// abstract class DevEnv {}
+
+// @Envify(path: '.env.production')
+// abstract class ProdEnv {}

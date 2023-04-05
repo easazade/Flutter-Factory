f553f7793516d04cb18afd9cda4383b9e3b95381
alireza easazade <easazade@gmail.com>
2023-04-05 20:36:38 +0330

    feat(analyzing_analyzer): Init project

---
+++ analying_analyzer/bin/analying_analyzer.dart
@@ -0,0 +1,5 @@
+import 'package:analying_analyzer/analying_analyzer.dart' as analying_analyzer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${analying_analyzer.calculate()}!');
+}
---
+++ analying_analyzer/test/analying_analyzer_test.dart
@@ -0,0 +1,8 @@
+import 'package:analying_analyzer/analying_analyzer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

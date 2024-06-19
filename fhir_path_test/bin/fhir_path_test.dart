3d31deff8a795d726f6029a69a2bf5fc8c937e31
alireza easazade <easazade@gmail.com>
2024-06-19 13:59:00 +0200

    chor(fhir_path_test): Init project

---
+++ fhir_path_test/bin/fhir_path_test.dart
@@ -0,0 +1,5 @@
+import 'package:fhir_path_test/fhir_path_test.dart' as fhir_path_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fhir_path_test.calculate()}!');
+}
---
+++ fhir_path_test/test/fhir_path_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:fhir_path_test/fhir_path_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

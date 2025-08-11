0d9b3e970f6f29bd92f696820ca1a267c927fc35
Ali <easazade@gmail.com>
2025-08-11 11:57:46 +0330

    feat(code_gen_build_runner): Add new project for notes about creating packges that works with build_runner

---
+++ code_gen_build_runner/bin/code_gen_build_runner.dart
@@ -0,0 +1,5 @@
+import 'package:code_gen_build_runner/code_gen_build_runner.dart' as code_gen_build_runner;
+
+void main(List<String> arguments) {
+  print('Hello world: ${code_gen_build_runner.calculate()}!');
+}
---
+++ code_gen_build_runner/test/code_gen_build_runner_test.dart
@@ -0,0 +1,8 @@
+import 'package:code_gen_build_runner/code_gen_build_runner.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
